4920b531aaab2fa515f16649a21fba526d1e7f23
pe7er <github@pe7er.com>
2016-10-12 10:04:38 +0200

    MQTT library added

--- wordpress/wp-content/plugins/db8iotlogincheck/db8iotlogincheck.php
+++ wordpress/wp-content/plugins/db8iotlogincheck/db8iotlogincheck.php
@@ -8,21 +8,21 @@
  * Author URI: http://db8.nl/
  * License: GNU General Public License version 2 or later
  *
-db8 IoT Login Check is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-any later version.
-
-db8 IoT Login Check is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with {Plugin Name}. If not, see {License URI}.
+ * db8 IoT Login Check is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * db8 IoT Login Check is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with {Plugin Name}. If not, see {License URI}.
  */
 
-defined( 'ABSPATH' ) or die( );
+defined('ABSPATH') or die();
 
 
 /* my first attempt */
@@ -40,10 +40,12 @@ function custom_login() {
 	//$creds['user_login'] = 'example';
 	//$creds['user_password'] = 'plaintextpw';
 	$creds['remember'] = false;
-	$user = wp_signon( $creds, false );
-	if ( is_wp_error($user) )
+	$user              = wp_signon( $creds, false );
+	if ( is_wp_error( $user ) ) {
 		echo "MQTT here" . $user->get_error_message();
+	}
 }
+
 // run it before the headers and cookies are sent
 add_action( 'after_setup_theme', 'custom_login' );
 
@@ -61,3 +63,38 @@ die("stop!");
 		die ('myError');
 	}
 */
+
+/**
+ * MQTT
+ *
+ * @return
+ */
+function MQTT()
+{
+	$MQTTBroker = $this->params->get( 'mqttbroker' );
+	$MQTTPort   = $this->params->get( 'mqttport' );
+	$MQTTClient = $this->params->get( 'mqttclient' );
+
+	require( "phpMQTT/phpMQTT.php" );
+
+	$MQTT = new phpMQTT( $MQTTBroker, $MQTTPort, $MQTTClient );
+
+	if ( $MQTT->connect() )
+	{
+		// Test
+		$channel = $this->params->get('channel');
+
+		// Default  QoS = 0
+		$QoS = $this->params->get('qos');
+
+		$MQTT->publish($channel, $errormsg, $QoS);
+
+		$extramessage = $this->params->get( 'extramessage' ); // red_on
+		if ( $extramessage ) {
+			$MQTT->publish( $channel, $extramessage, $QoS );
+		}
+
+		$MQTT->close();
+	}
+
+}
